4f3a7b0c87beafca6fccc906d0e601ba4afd963e
AngryDoggaebi <aofhoa@gmail.com>
2023-10-24 16:09:01 +0900

    mainpage css 수정

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Link from 'next/link';
 
 const index = () => {
   return (
-    <div>
-      <div>재고관리, 인수인계 어플리케이션</div>
-      <Link href={'/inventory'}>
-        <button>바로가기</button>
-      </Link>
-    </div>
+    <main>
+      <div className="home">
+        <h1 className="home-title">재고관리, 인수인계 어플리케이션</h1>
+        <Link href={'/inventory'}>
+          <button className="home-button">바로가기</button>
+        </Link>
+        <span>* 달력을 통해 이전 데이터를 조회할 수 있습니다</span>
+        <span>* 데이터 삭제는 당일 데이터에 한해 가능합니다</span>
+      </div>
+    </main>
   );
 };
 
